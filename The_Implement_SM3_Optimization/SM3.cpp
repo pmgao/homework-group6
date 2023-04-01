0c21aec5ac6e95c54d127c1831f4c9570428d270
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-04-01 11:00:08 +0800

    Create SM3.cpp

---
+++ The_Implement_SM3_Optimization/SM3.cpp
@@ -0,0 +1,167 @@
+#include<stdint.h>
+#include<string.h>
+#include<stdio.h>
+
+#define MAX_LEN 2<<12
+#define sm3_digest_BYTES 32
+#define sm3_block_BYTES 64
+#define sm3_hmac_BYTES sm3_diget_BYTES
+#define rol(x,j) ((x<<j)|(x>>(32-j)))
+#define P0(x) ((x) ^ rol((x), 9) ^ rol((x), 17))
+#define P1(x) ((x) ^ rol((x), 15) ^ rol((x), 23))
+#define FF0(x, y, z) ((x) ^ (y) ^ (z))
+#define FF1(x, y, z) (((x) & (y)) | ((x) & (z)) | ((y) & (z)))
+#define GG0(x, y, z) ((x) ^ (y) ^ (z))
+#define GG1(x, y, z) (((x) & (y)) | ((~(x)) & (z)))
+#define byte_swap32(x) ((x & 0xff000000) >> 24) |((x & 0x00ff0000) >> 8) |((x & 0x0000ff00) << 8) |((x & 0x000000ff) << 24)
+#define byte_swap64(x) ((x & 0xff00000000000000ull) >> 56) |\
+((x & 0x00ff000000000000ull) >> 40) |\
+((x & 0x0000ff0000000000ull) >> 24) |\
+((x & 0x000000ff00000000ull) >> 8) |\
+((x & 0x00000000ff000000ull) << 8) |\
+((x & 0x0000000000ff0000ull) << 24) |\
+((x & 0x000000000000ff00ull) << 40) |\
+((x & 0x00000000000000ffull) << 56)
+
+typedef struct sm3_ctx_t {
+	uint32_t digest[sm3_digest_BYTES / sizeof(uint32_t)];
+	int nblocks; //number of blocks that have been processed
+	uint8_t block[sm3_block_BYTES * 4];
+	int num;
+} sm3_ctx;
+
+void sm3_init(sm3_ctx* ctx);
+void sm3_update(sm3_ctx* ctx, const uint8_t* data, size_t data_len);
+void sm3_final(sm3_ctx* ctx, uint8_t* digest);
+void sm3(const uint8_t* message, size_t mlen, uint8_t res[sm3_block_BYTES]);
+static void sm3_compress(uint32_t digest[sm3_block_BYTES / sizeof(uint32_t)], const uint8_t block[sm3_block_BYTES]);
+
+
+void sm3_init(sm3_ctx* ctx) {
+	ctx->digest[0] = 0x7380166F;
+	ctx->digest[1] = 0x4914B2B9;
+	ctx->digest[2] = 0x172442D7;
+	ctx->digest[3] = 0xDA8A0600;
+	ctx->digest[4] = 0xA96F30BC;
+	ctx->digest[5] = 0x163138AA;
+	ctx->digest[6] = 0xE38DEE4D;
+	ctx->digest[7] = 0xB0FB0E4E;
+
+	ctx->nblocks = 0;
+	ctx->num = 0;
+}
+
+void sm3_update(sm3_ctx* ctx, const uint8_t* data, size_t dlen) {
+	if (ctx->num) {
+		unsigned int left = sm3_block_BYTES - ctx->num;
+		if (dlen < left) {
+			memcpy(ctx->block + ctx->num, data, dlen);
+			ctx->num += dlen;
+			return;
+		}
+		else {
+			memcpy(ctx->block + ctx->num, data, left);
+			sm3_compress(ctx->digest, ctx->block);
+			ctx->nblocks++;
+			data += left;
+			dlen -= left;
+		}
+	}
+	while (dlen >= sm3_block_BYTES) {
+		sm3_compress(ctx->digest, data);
+		ctx->nblocks++;
+		data += sm3_block_BYTES;
+		dlen -= sm3_block_BYTES;
+	}
+	ctx->num = dlen;
+	if (dlen) {
+		memcpy(ctx->block, data, dlen);
+	}
+}
+
+void sm3_final(sm3_ctx* ctx, uint8_t* digest) {
+	size_t i;
+	uint32_t* pdigest = (uint32_t*)(digest);
+	uint64_t* count = (uint64_t*)(ctx->block + sm3_block_BYTES - 8);
+
+	ctx->block[ctx->num] = 0x80;
+
+	if (ctx->num + 9 <= sm3_block_BYTES) {
+		memset(ctx->block + ctx->num + 1, 0, sm3_block_BYTES - ctx->num - 9);
+	}
+	else {
+		memset(ctx->block + ctx->num + 1, 0, sm3_block_BYTES - ctx->num - 1);
+		sm3_compress(ctx->digest, ctx->block);
+		memset(ctx->block, 0, sm3_block_BYTES - 8);
+	}
+
+	count[0] = (uint64_t)(ctx->nblocks) * 512 + (ctx->num << 3);
+	count[0] = byte_swap64(count[0]);
+
+	sm3_compress(ctx->digest, ctx->block);
+	for (i = 0; i < sizeof(ctx->digest) / sizeof(ctx->digest[0]); i++) {
+		pdigest[i] = byte_swap32(ctx->digest[i]);
+	}
+}
+
+static void sm3_compress(uint32_t digest[sm3_block_BYTES / sizeof(uint32_t)], const uint8_t block[sm3_block_BYTES]) {
+	int j;
+	uint32_t W[68], W1[64];
+	const uint32_t* pblock = (const uint32_t*)(block);
+	uint32_t A = digest[0], B = digest[1], C = digest[2], D = digest[3];
+	uint32_t E = digest[4], F = digest[5], G = digest[6], H = digest[7];
+
+	uint32_t SS1, SS2, TT1, TT2, T[64];
+
+	for (j = 0; j < 16; j++)W[j] = byte_swap32(pblock[j]);
+	for (j = 16; j < 68; j++)
+		W[j] = P1(W[j - 16] ^ W[j - 9] ^ rol(W[j - 3], 15)) ^ rol(W[j - 13], 7) ^ W[j - 6];
+	for (j = 0; j < 64; j++)W1[j] = W[j] ^ W[j + 4];
+
+	for (j = 0; j < 16; j++) {
+		T[j] = 0x79CC4519;
+		SS1 = rol((rol(A, 12) + E + rol(T[j], j)), 7);
+		SS2 = SS1 ^ rol(A, 12);
+		TT1 = FF0(A, B, C) + D + SS2 + W1[j];
+		TT2 = GG0(E, F, G) + H + SS1 + W[j];
+		D = C, C = rol(B, 9), B = A, A = TT1;
+		H = G, G = rol(F, 19), F = E, E = P0(TT2);
+	}
+	for (j = 16; j < 64; j++) {
+		T[j] = 0x7A879D8A;
+		SS1 = rol((rol(A, 12) + E + rol(T[j], j)), 7);
+		SS2 = SS1 ^ rol(A, 12);
+		TT1 = FF1(A, B, C) + D + SS2 + W1[j];
+		TT2 = GG1(E, F, G) + H + SS1 + W[j];
+		D = C, C = rol(B, 9), B = A, A = TT1;
+		H = G, G = rol(F, 19), F = E, E = P0(TT2);
+	}
+	digest[0] ^= A, digest[1] ^= B, digest[2] ^= C, digest[3] ^= D;
+	digest[4] ^= E, digest[5] ^= F, digest[6] ^= G, digest[7] ^= H;
+}
+
+
+void sm3(const uint8_t* message, size_t mlen, uint8_t res[sm3_block_BYTES])
+{
+	sm3_ctx ctx;
+	sm3_init(&ctx);
+	sm3_update(&ctx, message, mlen);
+	sm3_final(&ctx, res);
+}
+
+int main() {
+	size_t len = 0;
+	uint8_t hash[32] = {};
+	uint8_t message[MAX_LEN] = "202100460055\0";
+
+	for (int i = 0; i < MAX_LEN; i++) {
+		if (message[i] == '\0')break;
+		len++;
+	}
+	
+	sm3(message, len, hash);
+	for (int i = 0; i < 32; i++)
+		printf("%02x", hash[i]);
+
+	return 0;
+}
