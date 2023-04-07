9aa903914f3a104b67866de45ffd1cd4e198549b
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-04-07 18:55:03 +0800

    Update Merkle_Tree.cpp

--- The_Implement_of_Merkle_Tree/Merkle_Tree.cpp
+++ The_Implement_of_Merkle_Tree/Merkle_Tree.cpp
@@ -12,60 +12,59 @@ static uint8_t* table[MAX_SIZE];
 
 
 void generate_data() {
-    for (size_t i = 0; i < MAX_SIZE; i++) {
-        srand(time(NULL));
-        table[i] = new uint8_t[length];
-        for (size_t j = 0; j < length; j++) {
-            table[i][j] = rand() % 255;
-        }
-    }
-   
+	for (size_t i = 0; i < MAX_SIZE; i++) {
+		srand(time(NULL));
+		table[i] = new uint8_t[length];
+		for (size_t j = 0; j < length; j++) {
+			table[i][j] = rand() % 255;
+		}
+	}
+
 }
 
 int main() {
-    generate_data();
-    clock_t t;
-    vector<char*> v(MAX_SIZE - TEST_INSERT);
+	generate_data();
+	clock_t t;
+	vector<char*> v(MAX_SIZE - TEST_INSERT);
 
-    for (int i = 0; i < MAX_SIZE - TEST_INSERT; i++) {
-        v[i] = (char*)table[i];
-    }
-
-   
-    t = clock();
-    merkletree mtree = merkletree(v, length);
-
-    printf("maked merkle\n");
+	for (int i = 0; i < MAX_SIZE - TEST_INSERT; i++) {
+		v[i] = (char*)table[i];
+	}
 
 
-    for (int i = MAX_SIZE - TEST_INSERT; i < MAX_SIZE; i++) {
-        v.push_back((char*)table[i]);
-        mtree.pushleaf(v[i]);
-    }
+	t = clock();
+	merkletree mtree = merkletree(v, length);
+
+	printf("maked merkle\n");
 
 
-    printf("root : %s\n", mtree.root());
-    t = clock() - t;
-    printf("[build mtree] took %d clocks (%f secs)\n", t, (float)t / CLOCKS_PER_SEC);
+	for (int i = MAX_SIZE - TEST_INSERT; i < MAX_SIZE; i++) {
+		v.push_back((char*)table[i]);
+		mtree.pushleaf(v[i]);
+	}
+
+	t = clock() - t;
+	printf("root: %s\n", mtree.root());
+	printf("[build mtree] took %d clocks (%f secs)\n", t, (float)t / CLOCKS_PER_SEC);
 
 
 
-    {
-        //Inclusion Proof
-        vector<ProofNode> proof = mtree.proof((char*)table[0]);
-        bool verproof = verifyProof((char*)table[0], mtree.root(), proof);
-        printf("[verify proof][ => %d ]\n", verproof);
-    }
+	{
+		//Inclusion Proof
+		vector<ProofNode> proof = mtree.proof((char*)table[0]);
+		bool verproof = verifyProof((char*)table[0], mtree.root(), proof);
+		printf("[verify proof][ => %d ]\n", verproof);
+	}
 
-    {
-        //Exclusion Proof
-        char* test;
-        test = new char[length] {0x01};
-        vector<ProofNode> proof = mtree.proof(test);
-        bool verproof = verifyProof(test, mtree.root(), proof);
-        printf("[verify proof][ => %d ]\n", verproof);
-        delete[] test;
-    }
+	{
+		//Exclusion Proof
+		char* test;
+		test = new char[length] {0x01};
+		vector<ProofNode> proof = mtree.proof(test);
+		bool verproof = verifyProof(test, mtree.root(), proof);
+		printf("[verify proof][ => %d ]\n", verproof);
+		delete[] test;
+	}
 
-    return 0;
+	return 0;
 }
