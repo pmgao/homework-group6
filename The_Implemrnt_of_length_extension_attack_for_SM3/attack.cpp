ed47c0dbdaa7bae7d823f9cf88dd31085fb48e1f
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-06-08 19:26:16 +0800

    Update attack.cpp

--- The_Implemrnt_of_length_extension_attack_for_SM3/attack.cpp
+++ The_Implemrnt_of_length_extension_attack_for_SM3/attack.cpp
@@ -4,7 +4,6 @@
 #include<fstream>
 #include<intrin.h>
 using namespace std;
-#pragma warning(disable:4996)
 
 int IV[8] = { 0x7380166f, 0x4914b2b9, 0x172442d7, 0xda8a0600, 0xa96f30bc, 0x163138aa, 0xe38dee4d ,0xb0fb0e4e };
 int IV2[8] = { 0x7380166f, 0x4914b2b9, 0x172442d7, 0xda8a0600, 0xa96f30bc, 0x163138aa, 0xe38dee4d ,0xb0fb0e4e };
