9b1fca2e67f8c3bd56cb1eb944f6feee62561f36
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-08 15:27:41 +0800

    Update attack.cpp

--- Project03: implement length extension attack for SM3, SHA256, etc./attack.cpp
+++ Project03: implement length extension attack for SM3, SHA256, etc./attack.cpp
@@ -12,7 +12,7 @@ void sm3_simd(char plaintext[], int* hash_val, int lenth_for_plaintext) {
 }
 
 
-void sm3_simd_for_length_attack(char plaintext[], int* hash_val, int lenth_for_plaintext, int length_for_message) {
+void sm3_for_length_attack(char plaintext[], int* hash_val, int lenth_for_plaintext, int length_for_message) {
     int n = bit_stuff_for_length_attack(plaintext, lenth_for_plaintext, length_for_message) / 64;
     for (int i = 0; i < n; i++) {
         CF_for_simd((uint32_t*)IV, (int*)&plaintext_after_stuffing[i * 64]);
@@ -26,7 +26,8 @@ void sm3_simd_for_length_attack(char plaintext[], int* hash_val, int lenth_for_p
 int sm3_length_attack(char* memappend, int* hash_val, int length_formemappend, int length_for_message) {
     int new_hash_val[8];
     memcpy(IV, hash_val, 32);
-    sm3_simd_for_length_attack(memappend, new_hash_val, length_formemappend, length_for_message);
+    sm3_for_length_attack(memappend, new_hash_val, length_formemappend, length_for_message);
+    printf("Hash value of constructed message:\n");
     dump_buf((char*)new_hash_val, 32);
     return 0;
 }
@@ -36,7 +37,9 @@ int main() {
     int hash_val[8];
     int hash_val2[8];
     sm3_simd(plaintext, hash_val, sizeof(plaintext));
+    printf("Hash value of origin message:\n");
     dump_buf((char*)hash_val, 32);
+
     for (int i = 0; i < 8; i++) {
         hash_val2[i] = byte_swap32(hash_val[i]);
     }
@@ -46,6 +49,7 @@ int main() {
     char memappend[] = "202100460055";
     memcpy(&plaintext_for_length_attack[64], memappend, sizeof(memappend));
     sm3_simd(plaintext_for_length_attack, hash_val, 64 + sizeof(memappend));
+    printf("Hash value of new message:\n");
     dump_buf((char*)hash_val, 32);
     sm3_length_attack(memappend, hash_val2, sizeof(memappend), 64);
 
