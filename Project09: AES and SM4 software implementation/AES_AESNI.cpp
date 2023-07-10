76c4df4a0feb7a0026671672ff111499ec4b8264
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-10 10:03:15 +0800

    Update AES_AESNI.cpp

--- Project09: AES and SM4 software implementation/AES_AESNI.cpp
+++ Project09: AES and SM4 software implementation/AES_AESNI.cpp
@@ -36,63 +36,62 @@ using std::endl;
     }while(0)
 
 static __m128i aes_128_key_expansion(__m128i key, __m128i keygened) {
-	keygened = _mm_shuffle_epi32(keygened, _MM_SHUFFLE(3, 3, 3, 3));
-	key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
-	key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
-	key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
-	return _mm_xor_si128(key, keygened);
+    keygened = _mm_shuffle_epi32(keygened, _MM_SHUFFLE(3, 3, 3, 3));
+    key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
+    key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
+    key = _mm_xor_si128(key, _mm_slli_si128(key, 4));
+    return _mm_xor_si128(key, keygened);
 }
 
 static void aes128_load_key_enc_only(uint8_t* enc_key, __m128i* key_schedule) {
-	key_schedule[0] = _mm_loadu_si128((const __m128i*) enc_key);
-	key_schedule[1] = AES_128_key_exp(key_schedule[0], 0x01);
-	key_schedule[2] = AES_128_key_exp(key_schedule[1], 0x02);
-	key_schedule[3] = AES_128_key_exp(key_schedule[2], 0x04);
-	key_schedule[4] = AES_128_key_exp(key_schedule[3], 0x08);
-	key_schedule[5] = AES_128_key_exp(key_schedule[4], 0x10);
-	key_schedule[6] = AES_128_key_exp(key_schedule[5], 0x20);
-	key_schedule[7] = AES_128_key_exp(key_schedule[6], 0x40);
-	key_schedule[8] = AES_128_key_exp(key_schedule[7], 0x80);
-	key_schedule[9] = AES_128_key_exp(key_schedule[8], 0x1B);
-	key_schedule[10] = AES_128_key_exp(key_schedule[9], 0x36);
+    key_schedule[0] = _mm_loadu_si128((const __m128i*) enc_key);
+    key_schedule[1] = AES_128_key_exp(key_schedule[0], 0x01);
+    key_schedule[2] = AES_128_key_exp(key_schedule[1], 0x02);
+    key_schedule[3] = AES_128_key_exp(key_schedule[2], 0x04);
+    key_schedule[4] = AES_128_key_exp(key_schedule[3], 0x08);
+    key_schedule[5] = AES_128_key_exp(key_schedule[4], 0x10);
+    key_schedule[6] = AES_128_key_exp(key_schedule[5], 0x20);
+    key_schedule[7] = AES_128_key_exp(key_schedule[6], 0x40);
+    key_schedule[8] = AES_128_key_exp(key_schedule[7], 0x80);
+    key_schedule[9] = AES_128_key_exp(key_schedule[8], 0x1B);
+    key_schedule[10] = AES_128_key_exp(key_schedule[9], 0x36);
 }
 
 static void aes128_load_key(uint8_t* enc_key, __m128i* key_schedule) {
-	aes128_load_key_enc_only(enc_key, key_schedule);
+    aes128_load_key_enc_only(enc_key, key_schedule);
 
-	key_schedule[11] = _mm_aesimc_si128(key_schedule[9]);
-	key_schedule[12] = _mm_aesimc_si128(key_schedule[8]);
-	key_schedule[13] = _mm_aesimc_si128(key_schedule[7]);
-	key_schedule[14] = _mm_aesimc_si128(key_schedule[6]);
-	key_schedule[15] = _mm_aesimc_si128(key_schedule[5]);
-	key_schedule[16] = _mm_aesimc_si128(key_schedule[4]);
-	key_schedule[17] = _mm_aesimc_si128(key_schedule[3]);
-	key_schedule[18] = _mm_aesimc_si128(key_schedule[2]);
-	key_schedule[19] = _mm_aesimc_si128(key_schedule[1]);
+    key_schedule[11] = _mm_aesimc_si128(key_schedule[9]);
+    key_schedule[12] = _mm_aesimc_si128(key_schedule[8]);
+    key_schedule[13] = _mm_aesimc_si128(key_schedule[7]);
+    key_schedule[14] = _mm_aesimc_si128(key_schedule[6]);
+    key_schedule[15] = _mm_aesimc_si128(key_schedule[5]);
+    key_schedule[16] = _mm_aesimc_si128(key_schedule[4]);
+    key_schedule[17] = _mm_aesimc_si128(key_schedule[3]);
+    key_schedule[18] = _mm_aesimc_si128(key_schedule[2]);
+    key_schedule[19] = _mm_aesimc_si128(key_schedule[1]);
 }
 
 static void aes128_enc(__m128i* key_schedule, uint8_t* plainText, uint8_t* cipherText) {
-	__m128i m = _mm_loadu_si128((__m128i*) plainText);
+    __m128i m = _mm_loadu_si128((__m128i*) plainText);
 
-	DO_ENC_BLOCK(m, key_schedule);
+    DO_ENC_BLOCK(m, key_schedule);
 
-	_mm_storeu_si128((__m128i*) cipherText, m);
+    _mm_storeu_si128((__m128i*) cipherText, m);
 }
 
 static void aes128_dec(__m128i* key_schedule, uint8_t* cipherText, uint8_t* plainText) {
-	__m128i m = _mm_loadu_si128((__m128i*) cipherText);
+    __m128i m = _mm_loadu_si128((__m128i*) cipherText);
 
-	DO_DEC_BLOCK(m, key_schedule);
+    DO_DEC_BLOCK(m, key_schedule);
 
-	_mm_storeu_si128((__m128i*) plainText, m);
+    _mm_storeu_si128((__m128i*) plainText, m);
 }
 
-void print_m128i_with_string(__m128i data) {
-	unsigned char* pointer = (unsigned char*)&data;
-
-	for (size_t i = 0; i < 16; i++)
-		printf("%02x ", pointer[i]);
-	printf("\n");
+void dump_buf(uint8_t data[]) {
+    for (size_t i = 0; i < 16; i++) {
+        cout << std::hex << (int)data[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
@@ -106,9 +105,9 @@ int main() {
     aes128_enc(key_schedule, plain, computed_cipher);
     aes128_dec(key_schedule, cipher, computed_plain);
 
-    int out = 0; //success
-    if (memcmp(cipher, computed_cipher, sizeof(cipher))) out = 1;
-    if (memcmp(plain, computed_plain, sizeof(plain))) out |= 2;
-    cout << out << endl;
+    cout << "Cipher text:";
+    dump_buf(computed_cipher);
+    cout << "Plain text:";
+    dump_buf(computed_plain);
     return 0;
 }
