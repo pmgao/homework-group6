4e0deae97441641b020f415c28993721d1301331
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-11 14:08:51 +0800

    Update SM2.py

--- Project11: impl sm2 with RFC6979/SM2.py
+++ Project11: impl sm2 with RFC6979/SM2.py
@@ -19,124 +19,107 @@ def Tonelli_Shanks(y, p):
     assert Legendre(y, p) == 1
     if p % 4 == 3:
         return pow(y, (p + 1) // 4, p)
-    q = p - 1
-    s = 0
+
+    q, s = p - 1, 0
     while q % 2 == 0:
-        q = q // 2
+        q //= 2
         s += 1
-    for z in range(2, p):
-        if Legendre(z, p) == p - 1:
-            c = pow(z, q, p)
-            break
+
+    z = next(z for z in range(2, p) if Legendre(z, p) == p - 1)
+    c = pow(z, q, p)
+
     r = pow(y, (q + 1) // 2, p)
     t = pow(y, q, p)
     m = s
+
     if t % p == 1:
         return r
-    else:
-        i = 0
-        while t % p != 1:
-            temp = pow(t, 2 ** (i + 1), p)
-            i += 1
-            if temp % p == 1:
-                b = pow(c, 2 ** (m - i - 1), p)
-                r = r * b % p
-                c = b * b % p
-                t = t * c % p
-                m = i
-                i = 0
-        return r
+
+    i = 0
+    while t % p != 1:
+        temp = pow(t, 2 ** (i + 1), p)
+        i += 1
+        if temp % p == 1:
+            b = pow(c, 2 ** (m - i - 1), p)
+            r = r * b % p
+            c = b * b % p
+            t = t * c % p
+            m = i
+            i = 0
+
+    return r
 
 
-def extended_euclidean_algorithm(j, k):
+def exgcd(j, k):
     if j == k:
-        return (j, 1, 0)
-    else:
-        i = 0
-        j_array = [j]
-        k_array = [k]
-        q_array = []
-        r_array = []
+        return j, 1, 0
 
-        prev_r_is_zero = False
+    j_array = [j]
+    k_array = [k]
+    q_array = []
+    r_array = []
 
-        while not (prev_r_is_zero):
-            q_array.append(k_array[i] // j_array[i])
-            r_array.append(k_array[i] % j_array[i])
-            k_array.append(j_array[i])
-            j_array.append(r_array[i])
-            i += 1
-            if r_array[i - 1] == 0:
-                prev_r_is_zero = True
-        i -= 1
-        gcd = j_array[i]
-        x_array = [1]
-        y_array = [0]
+    while True:
+        q = k_array[-1] // j_array[-1]
+        r = k_array[-1] % j_array[-1]
+        q_array.append(q)
+        r_array.append(r)
+        k_array.append(j_array[-1])
+        j_array.append(r)
+        if r == 0:
+            break
 
-        i -= 1
-        total_steps = i
+    gcd = j_array[-1]
+    x_array = [1]
+    y_array = [0]
+    total_steps = len(j_array) - 2
 
-        while i >= 0:
-            y_array.append(x_array[total_steps - i])
-            x_array.append(y_array[total_steps - i] - q_array[i] * x_array[total_steps - i])
-            i -= 1
+    for i in range(total_steps, -1, -1):
+        y_array.append(x_array[total_steps - i])
+        x_array.append(y_array[total_steps - i] - q_array[i] * x_array[total_steps - i])
 
-        return (gcd, x_array[-1], y_array[-1])
+    return gcd, x_array[-1], y_array[-1]
 
 
 def mod_inverse(j, n):
-    (gcd, x, y) = extended_euclidean_algorithm(j, n)
-
-    if gcd == 1:
-        return x % n
-    else:
-        return -1
+    gcd, x, _ = exgcd(j, n)
+    return x % n if gcd == 1 else -1
 
 
 def elliptic_add(p, q):
-    if p == 0 and q == 0:
-        return 0
-    elif p == 0:
+    if p == 0:
         return q
     elif q == 0:
         return p
     else:
         # Swap p and q if px > qx.
         if p[0] > q[0]:
-            temp = p
-            p = q
-            q = temp
-        r = []
+            p, q = q, p
         slope = (q[1] - p[1]) * mod_inverse(q[0] - p[0], P) % P
 
-        r.append((slope ** 2 - p[0] - q[0]) % P)
-        r.append((slope * (p[0] - r[0]) - p[1]) % P)
+        x = (slope ** 2 - p[0] - q[0]) % P
+        y = (slope * (p[0] - x) - p[1]) % P
 
-        return r[0], r[1]
+        return x, y
 
 
 def elliptic_double(p):
-    r = []
-
     slope = (3 * p[0] ** 2 + A) * mod_inverse(2 * p[1], P) % P
 
-    r.append((slope ** 2 - 2 * p[0]) % P)
-    r.append((slope * (p[0] - r[0]) - p[1]) % P)
+    x = (slope ** 2 - 2 * p[0]) % P
+    y = (slope * (p[0] - x) - p[1]) % P
 
-    return r[0], r[1]
+    return x, y
 
 
 def elliptic_multiply(s, p):
-    n = p
-    r = 0  # 0 representing a point at infinity
+    r = (0, 0)  # 0 representing a point at infinity
 
-    s_binary = bin(s)[2:]  # convert s to binary and remove the "0b" in the beginning
-    s_length = len(s_binary)
-
-    for i in reversed(range(s_length)):
-        if s_binary[i] == '1':
-            r = elliptic_add(r, n)
-        n = elliptic_double(n)
+    while s > 0:
+        if s & 1:
+            r = elliptic_add(r, p)
+        p = elliptic_double(p)
+        s >>= 1
 
     return r
 
@@ -144,23 +127,14 @@ def elliptic_multiply(s, p):
 def get_bit_num(x):
     if isinstance(x, int):
         num = 0
-        tmp = x >> 64
-        while tmp:
-            num += 64
-            tmp >>= 64
-        tmp = x >> num >> 8
-        while tmp:
-            num += 8
-            tmp >>= 8
-        x >>= num
         while x:
             num += 1
             x >>= 1
         return num
     elif isinstance(x, str):
-        return len(x.encode()) << 3
+        return len(x.encode()) * 8
     elif isinstance(x, bytes):
-        return len(x) << 3
+        return len(x) * 8
     return 0
 
 
@@ -228,8 +202,9 @@ def verify(public_key, ID, message, signature):
 if __name__ == "__main__":
     prikey, pubkey = generate_key()
     print('pk：', pubkey)
-    message = input("message = ")
-    ID = input("ID = ")
+    message = input("Input your message: ")
+    ID = input("Input your ID: ")
+
     Z_A = precompute(ID, A, B, G_X, G_Y, pubkey[0], pubkey[1])
     signature = sign(prikey, message, str(Z_A))
     while signature is None:
@@ -237,6 +212,7 @@ if __name__ == "__main__":
         print('new pk：', pubkey)
         Z_A = precompute(ID, A, B, G_X, G_Y, pubkey[0], pubkey[1])
         signature = sign(prikey, message, str(Z_A))
+
     print("sign: ", signature)
     if verify(pubkey, ID, message, signature) == 1:
         print('verified!')
