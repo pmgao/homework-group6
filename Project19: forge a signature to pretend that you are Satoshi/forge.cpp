7746d95417de88b6d74b81ed687826859411d8c0
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-13 11:56:33 +0800

    Update forge.cpp

--- Project19: forge a signature to pretend that you are Satoshi/forge.cpp
+++ Project19: forge a signature to pretend that you are Satoshi/forge.cpp
@@ -4,65 +4,84 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <stdio.h>
+#include <iostream>
 
-EC_GROUP* group = EC_GROUP_new_by_curve_name(NID_secp256k1);
-EC_POINT* G = EC_POINT_new(group);
-BIGNUM* xG = BN_new();
-BIGNUM* yG = BN_new();
-BIGNUM* order = BN_new();
-unsigned char* pubkey = NULL;
-int pubkey_len = 0;
-EC_KEY* key = EC_KEY_new();
-EC_POINT* P = EC_POINT_new(group);
+using namespace std;
+
+const BIGNUM* N;
+const EC_POINT* G;
+EC_GROUP* curve = EC_GROUP_new_by_curve_name(NID_secp256k1);
+EC_POINT* PK = EC_POINT_new(curve);
+BIGNUM* SK = BN_new();
+
 
 void init() {
-	BN_hex2bn(&xG, "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798");
-	BN_hex2bn(&yG, "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8");
-	EC_POINT_set_affine_coordinates_GFp(group, G, xG, yG, NULL);
+	N = EC_GROUP_get0_order(curve);
+	G = EC_GROUP_get0_generator(curve);
+	BN_hex2bn(&SK, "0x83bb90c52a7aabc2cfc4eaf90bd32e19894454d1f0e8e526a936f47cfd2a1e91");
+	EC_POINT_mul(curve, PK, SK, nullptr, nullptr, BN_CTX_new());
+}
 
-	BN_hex2bn(&order, "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
-	const char* GENESIS_BLOCK_PUBKEY = "04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f";
-	EC_KEY_set_group(key, group);
+bool verify(BIGNUM* e_,BIGNUM* r_,BIGNUM* s_) {
+    BIGNUM* w = BN_new();
+    BN_mod_inverse(w, s_, N, BN_CTX_new());
+
+    BIGNUM* temp1 = BN_new(), * temp2 = BN_new();
+    BN_mul(temp1, e_, w, BN_CTX_new());
+    BN_mul(temp2, r_, w, BN_CTX_new());
+
+    EC_POINT* temp3 = EC_POINT_new(curve);
+    EC_POINT_mul(curve, temp3, temp1, PK, temp2, BN_CTX_new());
+
+    BIGNUM* x = BN_new();
+    EC_POINT_get_affine_coordinates_GFp(curve, temp3, x, nullptr, NULL);
+    return BN_cmp(x, r_);
 }
 
 void forge() {
-	EC_KEY_oct2key(key, pubkey, pubkey_len, NULL);
-	EC_POINT_oct2point(group, P, pubkey, pubkey_len, NULL);
+    BIGNUM* u = BN_new();
+    BIGNUM* v = BN_new();
+    BIGNUM* gcd = BN_new();
 
-	BIGNUM* u = BN_new();
-	BIGNUM* v = BN_new();
-	BN_rand_range(u, order);
-	BN_rand_range(v, order);
+    BN_rand(u, BN_num_bits(N), 0, 0);
+    while (1) {
+        BN_gcd(gcd, u, N, BN_CTX_new());
+        if (BN_is_one(gcd)) {
+            break;
+        }
+        BN_rand(u, BN_num_bits(N), 0, 0);
+    }
+    BN_rand(v, BN_num_bits(N), 0, 0);
+    while (1) {
+        BN_gcd(gcd, v, N, BN_CTX_new());
+        if (BN_is_one(gcd)) {
+            break;
+        }
+        BN_rand(v, BN_num_bits(N), 0, 0);
+    }
 
-	EC_POINT* R_ = EC_POINT_new(group);
-	EC_POINT* uG = EC_POINT_new(group);
-	EC_POINT* vP = EC_POINT_new(group);
-	EC_POINT_mul(group, uG, u, G, NULL, BN_CTX_new());
-	EC_POINT_mul(group, vP, v, P, NULL, BN_CTX_new());
-	EC_POINT_add(group, R_, uG, vP, NULL);
+    EC_POINT* R_ = EC_POINT_new(curve);
+    EC_POINT_mul(curve, R_, u, PK, v, BN_CTX_new());
 
-	BIGNUM* r_ = BN_new();
-	BIGNUM* xR = BN_new();
-	BIGNUM* yR = BN_new();
-	EC_POINT_get_affine_coordinates_GFp(group, R_, xR, yR, NULL);
-	BN_mod(r_, xR, order, BN_CTX_new());
+    BIGNUM* r_ = BN_new();
+    EC_POINT_get_affine_coordinates_GFp(curve, R_, r_, nullptr, NULL);
 
-	BIGNUM* e_ = BN_new();
-	BIGNUM* u_inverse = BN_new();
-	BN_mod_inverse(u_inverse, u, order, NULL);
-	BIGNUM* r_u_v_inverse = BN_new();
-	BN_mod_mul(r_u_v_inverse, r_, u_inverse, order, BN_CTX_new());
-	BN_mod_mul(e_, r_u_v_inverse, v, order, BN_CTX_new());
-	BIGNUM* s_ = BN_new();
-	BN_mod_inverse(s_, v, order, NULL);
+    BIGNUM* s_ = BN_new();
+    BN_mod_inverse(s_, v, N, BN_CTX_new());
+    BN_mod_mul(s_, s_, r_, N, BN_CTX_new());
 
-	char* r_str = BN_bn2hex(r_);
-	char* s_str = BN_bn2hex(s_);
+    BIGNUM* e_ = BN_new();
+    BN_mod_inverse(e_, v, N, BN_CTX_new());
+    BN_mod_mul(e_, e_, u, N, BN_CTX_new());
+    BN_mod_mul(e_, e_, r_, N, BN_CTX_new());
 
-	printf("sigma = (%s, %s)\n", r_str, s_str);
-
-	OPENSSL_free(r_str);
-	OPENSSL_free(s_str);
+    cout << "forged signature:(" << BN_bn2hex(r_) << ", " << BN_bn2hex(s_) << ")\n";
+    if (!verify(e_, r_, s_)) {
+        cout << "verified success!\n";
+    }
+    else {
+        cout << "verified failed!\n";
+    }
 }
 
 int main() {
