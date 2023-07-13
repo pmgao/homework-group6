0e9f97d1d5255fc2367b72a70822c3ea5301c215
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-13 17:22:54 +0800

    Update ECMH.py

--- Project13: Implement the above ECMH scheme/ECMH.py
+++ Project13: Implement the above ECMH scheme/ECMH.py
@@ -29,40 +29,33 @@ def inv(a, n):
         return -1
 
 
-def EC_add(p, q):
-    if p == 0 and q == 0:
-        return 0  # 0 + 0 = 0
-    elif p == 0:
-        return q  # 0 + q = q
+def elliptic_add(p, q):
+    if p == 0:
+        return q
     elif q == 0:
-        return p  # p + 0 = p
+        return p
     else:
-        if p[0] == q[0]:
-            if (p[1] + q[1]) % P == 0:
-                return 0  # mutually inverse
-            elif p[1] == q[1]:
-                return EC_double(p)
-        elif p[0] > q[0]:  # swap if px > qx
-            tmp = p
-            p = q
-            q = tmp
-        r = []
-        slope = (q[1] - p[1]) * inv(q[0] - p[0], P) % P  # 斜率
-        r.append((slope ** 2 - p[0] - q[0]) % P)
-        r.append((slope * (p[0] - r[0]) - p[1]) % P)
-        return (r[0], r[1])
+        # Swap p and q if px > qx.
+        if p[0] > q[0]:
+            p, q = q, p
+        slope = (q[1] - p[1]) * inv(q[0] - p[0], P) % P
+
+        x = (slope ** 2 - p[0] - q[0]) % P
+        y = (slope * (p[0] - x) - p[1]) % P
+
+        return x, y
 
 
-def EC_inv(p):
+def elliptic_inv(p):
     return [p[0], P - p[1]]
 
 
-def EC_sub(p, q):
-    return EC_add(p, EC_inv(q))
+def elliptic_sub(p, q):
+    return elliptic_add(p, elliptic_inv(q))
 
 
 def ADD(ecmh, msg):
-    return EC_add(ecmh, msg_to_dot(msg))
+    return elliptic_add(ecmh, msg_to_dot(msg))
 
 
 def EC_double(p):
@@ -73,12 +66,11 @@ def EC_double(p):
     return (r[0], r[1])
 
 
-def msg_to_dot(msg):  # 映射到椭圆曲线上的点
+def msg_to_dot(msg):
     def Legendre(y, p):
         return pow(y, (p - 1) // 2, p)
 
     def msg_to_x(m):
-        """消息转换成横坐标"""
         mdigest = sha256(bytes(bytearray(m, encoding='utf-8'))).hexdigest()
         while 1:  # cycle until x belong to QR
             x = int(mdigest, 16)
@@ -87,9 +79,7 @@ def msg_to_dot(msg):  # 映射到椭圆曲线上的点
             mdigest = sha256(bytes(bytearray(m, encoding='utf-8'))).hexdigest()
         return x
 
-    def get_y(x):  # y^2 = x^3 + 7 mod P
-        """y ** 2 = x ** 3 + 7
-        已知横坐标，计算纵坐标"""
+    def get_y(x):
         right = (x ** 3 + 7) % P
         while 1:
             a = randint(0, P)
@@ -110,7 +100,7 @@ def single(msg):
 
 
 def remove(ecmh, msg):
-    return EC_sub(ecmh, msg_to_dot(msg))
+    return elliptic_sub(ecmh, msg_to_dot(msg))
 
 
 def combine(msg_set):
@@ -125,9 +115,9 @@ if __name__ == "__main__":
     m1 = "202100460055"
     m2 = "1234567890"
 
-    print('single(m1)\t\t\t', single(m1))
-    print('single(m2)\t\t\t', single(m2))
-    print('combine([m1, m2])\t', combine([m1, m2]))
-    print('combine([m2, m1])\t', combine([m2, m1]))
-    print('ADD(single(m1), m2))', ADD(single(m1), m2))
-    print('remove(combine([m1, m2]), m2)', remove(combine([m1, m2]), m2))
+    print('HASH(m1)\t\t\t', single(m1))
+    print('HASH(m2)\t\t\t', single(m2))
+    print('HASH([m1, m2])\t\t', combine([m1, m2]))
+    print('HASH([m2, m1])\t\t', combine([m2, m1]))
+    print('HASH(single(m1) + m2))', ADD(single(m1), m2))
+    print('HASH(combine([m1, m2]) + m2)', remove(combine([m1, m2]), m2))
