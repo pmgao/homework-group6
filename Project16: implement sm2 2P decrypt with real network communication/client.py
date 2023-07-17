785ce144346418b3c6e43166c58077e2a60973af
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-17 16:29:25 +0800

    Update client.py

--- Project16: implement sm2 2P decrypt with real network communication/client.py
+++ Project16: implement sm2 2P decrypt with real network communication/client.py
@@ -6,36 +6,32 @@ from sm2 import *
 
 
 def Gen_Key():
-    d1 = random.randint(0, N - 1)  # sk
-    tmp = mod_inverse(d1, N)
-    P1 = elliptic_multiply(tmp, G)  # pk
+    d1 = random.randint(0, N - 1)
+    P1 = elliptic_multiply(inv(d1, N), G)
     return d1, P1
 
 
 def C_to_T1(d1, C1):
     if C1 == 0: return 'error'
-    tmp = mod_inverse(d1, N)
-    T1 = elliptic_multiply(tmp, C1)
+    T1 = elliptic_multiply(inv(d1, N), C1)
     return T1
 
 
 def dec(T2, C1, C2, C3):
-    tmp = elliptic_sub(T2, C1)  # kP = (x2, y2)
+    tmp = elliptic_sub(T2, C1)
     x2 = hex(tmp[0])[2:]
     y2 = hex(tmp[1])[2:]
     klen = len(C2) * 4
     t = KDF(x2 + y2, klen)
-    M_ = dec_XOR(C2, t)  # M_为M'
+    M_ = dec_XOR(C2, t)
     u = sm3.sm3_hash(func.bytes_to_list(bytes((x2 + M_ + y2), encoding='utf-8')))
     if u != C3: return 'error:u != C3'
     return M_
 
 
 if __name__ == "__main__":
-    # 建立链接
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Gen_Key
     d1, P1 = Gen_Key()
     data = str(P1[0]) + '||' + str(P1[1])
     s.sendto(data.encode(), ("127.0.0.1", 12300))
@@ -45,7 +41,6 @@ if __name__ == "__main__":
     pk = (int(data[:flag]), int(data[flag + 2:]))
     print("pk: ", pk)
 
-    # C_to_T1
     M = input("M: ")
     C1, C2, C3 = SM2_enc(M, pk)
     print("cipher text:", C1[0], "||", C2, "||", C3)
@@ -53,7 +48,6 @@ if __name__ == "__main__":
     data = str(T1[0]) + '||' + str(T1[1])
     s.sendto(data.encode(), addr)
 
-    # dec
     data, addr = s.recvfrom(1024)
     data = data.decode()
     flag = data.index('||')
@@ -61,4 +55,4 @@ if __name__ == "__main__":
     M_ = dec(T2, C1, C2, C3)
     print("解密结果: ", M_)
     s.close()
-    print("client finished...")
+    print("client finished")
