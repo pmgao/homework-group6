0ff8451a9a7ad4ef2fc5b0a788d9af90fa16fb36
PeiminGao-group6num1 <92631121+PeiminGao-group6num1@users.noreply.github.com>
2023-07-17 16:12:14 +0800

    Update sm2.py

--- Project16: implement sm2 2P decrypt with real network communication/sm2.py
+++ Project16: implement sm2 2P decrypt with real network communication/sm2.py
@@ -1,82 +1,35 @@
 import secrets
 from gmssl import sm3, func
 
-# y^2=x^3+7
-A = 0
-B = 7
-P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
-N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
-G_X = 55066263022277343669578718895168534326250603453777594175500187360389116729240
-G_Y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+A = 0x787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498
+B = 0x63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A
+P = 0x8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3
+N = 0x8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7
+G_X = 0x421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D
+G_Y = 0x0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2
 G = (G_X, G_Y)
 h = 1
 
 
-def extended_euclidean_algorithm(j, k):
-    if j == k:
-        return (j, 1, 0)
-    else:
-        i = 0
-        j_array = [j]
-        k_array = [k]
-        q_array = []
-        r_array = []
-
-        prev_r_is_zero = False
-
-        while not (prev_r_is_zero):
-            q_array.append(k_array[i] // j_array[i])
-            r_array.append(k_array[i] % j_array[i])
-            k_array.append(j_array[i])
-            j_array.append(r_array[i])
-            i += 1
-            if r_array[i - 1] == 0:
-                prev_r_is_zero = True
-        i -= 1
-        gcd = j_array[i]
-        x_array = [1]
-        y_array = [0]
-
-        i -= 1
-        total_steps = i
-
-        while i >= 0:
-            y_array.append(x_array[total_steps - i])
-            x_array.append(y_array[total_steps - i] - q_array[i] * x_array[total_steps - i])
-            i -= 1
-
-        return (gcd, x_array[-1], y_array[-1])
+def inv(a, n):  # Extended Euclidean Algorithm/'division' in elliptic curves
+    lm, hm = 1, 0
+    low, high = a % n, n
+    while low > 1:
+        ratio = high // low
+        nm, new = hm - lm * ratio, high - low * ratio
+        lm, low, hm, high = nm, new, lm, low
+    return lm % n
 
 
-def mod_inverse(j, n):
-    (gcd, x, y) = extended_euclidean_algorithm(j, n)
-
-    if gcd == 1:
-        return x % n
-    else:
-        return -1
-
-
-def elliptic_add(p, q):
-    if p == 0 and q == 0:
-        return 0
-    elif p == 0:
-        return q
-    elif q == 0:
-        return p
-    else:
-        # Swap p and q if px > qx.
-        if p[0] > q[0]:
-            temp = p
-            p = q
-            q = temp
-        r = []
-        slope = (q[1] - p[1]) * mod_inverse(q[0] - p[0], P) % P
-
-        r.append((slope ** 2 - p[0] - q[0]) % P)
-        r.append((slope * (p[0] - r[0]) - p[1]) % P)
-
-        return r[0], r[1]
+def elliptic_add(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    LamAdd = ((b[1] - a[1]) * inv(b[0] - a[0], P)) % P
+    x = (LamAdd * LamAdd - a[0] - b[0]) % P
+    y = (LamAdd * (a[0] - x) - a[1]) % P
+    return (x, y)
 
 
 def elliptic_inv(p):
@@ -90,52 +43,35 @@ def elliptic_sub(p, q):
     return elliptic_add(p, q_inv)
 
 
-def elliptic_double(p):
-    r = []
-
-    slope = (3 * p[0] ** 2 + A) * mod_inverse(2 * p[1], P) % P
-
-    r.append((slope ** 2 - 2 * p[0]) % P)
-    r.append((slope * (p[0] - r[0]) - p[1]) % P)
-
-    return r[0], r[1]
+def elliptic_double(a):
+    Lam = ((3 * a[0] * a[0] + A) * inv((2 * a[1]), P)) % P
+    x = (Lam * Lam - 2 * a[0]) % P
+    y = (Lam * (a[0] - x) - a[1]) % P
+    return (x, y)
 
 
-def elliptic_multiply(s, p):
-    n = p
-    r = 0  # 0 representing a point at infinity
-
-    s_binary = bin(s)[2:]  # convert s to binary and remove the "0b" in the beginning
-    s_length = len(s_binary)
-
-    for i in reversed(range(s_length)):
-        if s_binary[i] == '1':
-            r = elliptic_add(r, n)
-        n = elliptic_double(n)
-
-    return r
+def elliptic_multiply(ScalarHex, GenPoint):
+    if ScalarHex == 0 or ScalarHex >= N: raise Exception("Invalid Scalar/Private Key")
+    ScalarBin = str(bin(ScalarHex))[2:]
+    Q = GenPoint
+    for i in range(1, len(ScalarBin)):
+        Q = elliptic_double(Q)
+        if ScalarBin[i] == "1":
+            Q = elliptic_add(Q, GenPoint)
+    return (Q)
 
 
 def get_bit_num(x):
     if isinstance(x, int):
         num = 0
-        tmp = x >> 64
-        while tmp:
-            num += 64
-            tmp >>= 64
-        tmp = x >> num >> 8
-        while tmp:
-            num += 8
-            tmp >>= 8
-        x >>= num
         while x:
             num += 1
             x >>= 1
         return num
     elif isinstance(x, str):
-        return len(x.encode()) << 3
+        return len(x.encode()) * 8
     elif isinstance(x, bytes):
-        return len(x) << 3
+        return len(x) * 8
     return 0
 
 
@@ -151,8 +87,8 @@ def KDF(Z, klen):
     if n >= 2 ** 32 - 1: return 'error'
     K = ''
     for i in range(n):
-        ct = (hex(5552 + 1)[2:]).rjust(32, '0')  # ct is 32 bit counter
-        tmp_b = bytes((Z + ct), encoding='utf-8')
+        ct = format(5552 + 1, 'x').rjust(32, '0')  # ct is 32 bit counter
+        tmp_b = (Z + ct).encode('utf-8')
         Kct = sm3.sm3_hash(func.bytes_to_list(tmp_b))
         K += Kct  # K is hex string
     bit_len = 256 * n
@@ -162,27 +98,16 @@ def KDF(Z, klen):
 
 
 def enc_XOR(m, t):
-    m = bytes(m, encoding='utf-8')
-    m = func.bytes_to_list(m)  # each element -> 8-bit
-    n = len(m)  # n bytes
-    ans = []
-    for i in range(n):
-        mm = m[i]
-        tt = int(t[8 * i:8 * (i + 1)], 2)
-        a = (hex(mm ^ tt)[2:]).rjust(2, '0')
-        ans.append(a)
+    m = m.encode('utf-8')
+    n = len(m)
+    ans = [format(m[i] ^ int(t[8 * i:8 * (i + 1)], 2), '02x') for i in range(n)]
     A = ''.join(ans)
-    # length of A is klen/4
     return A
 
 
 def dec_XOR(C2, t):
     n = len(C2) // 2
-    ans = []
-    for i in range(n):
-        c2c2 = int(C2[2 * i:2 * (i + 1)], 16)  # -> int
-        tt = int(t[8 * i:8 * (i + 1)], 2)
-        ans.append(chr(c2c2 ^ tt))
+    ans = [chr(int(C2[2 * i:2 * (i + 1)], 16) ^ int(t[8 * i:8 * (i + 1)], 2)) for i in range(n)]
     A = ''.join(ans)
     return A
 
@@ -191,13 +116,13 @@ def SM2_enc(M, pk):
     if pk == 0: return 'error:public key'
     while 1:
         k = secrets.randbelow(N)
-        C1 = elliptic_multiply(k, G)  # C1 = kG = (x1, y1)
-        dot = elliptic_multiply(k, pk)  # kpk = (x2, y2)
+        C1 = elliptic_multiply(k, G)
+        dot = elliptic_multiply(k, pk)
         klen = get_bit_num(M)
         x2 = hex(dot[0])[2:]
         y2 = hex(dot[1])[2:]
         t = KDF(x2 + y2, klen)
-        if (t != '0' * klen):  # all '0' is invallid
+        if (t != '0' * klen):
             break
     C2 = enc_XOR(M, t)
     tmp_b = bytes((x2 + M + y2), encoding='utf-8')
