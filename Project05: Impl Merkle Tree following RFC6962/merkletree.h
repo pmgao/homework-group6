c0a01ed916707effe189bd7d5222de27727ec568
Borpord <92631121+Borpord@users.noreply.github.com>
2023-07-24 18:19:22 +0800

    Update merkletree.h

--- Project05: Impl Merkle Tree following RFC6962/merkletree.h
+++ Project05: Impl Merkle Tree following RFC6962/merkletree.h
@@ -2,30 +2,25 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-#define SHA256_LENGTH 64
-
 struct ProofNode {
 	char* left, * right, * parent;
 	ProofNode() :left(NULL), right(NULL), parent(NULL) {}
 	ProofNode(char* _left, char* _right, char* _parent) :left(_left), right(_right), parent(_parent) {}
 };
 
-
-// combin and hash by sha256
 static void combin(char* leftData, char* rightData, char out_buff[65]) {
 
 	unsigned char hash[SHA256_DIGEST_LENGTH];
-	SHA256_CTX sha256;
-	SHA256_Init(&sha256);
-	SHA256_Update(&sha256, leftData, strlen((const char*)leftData));
-	SHA256_Update(&sha256, rightData, strlen((const char*)rightData));
-	SHA256_Final(hash, &sha256);
+	SHA256_CTX digest;
+	SHA256_Init(&digest);
+	SHA256_Update(&digest, leftData, strlen((const char*)leftData));
+	SHA256_Update(&digest, rightData, strlen((const char*)rightData));
+	SHA256_Final(hash, &digest);
 
-	for (size_t i = 0; i < SHA256_DIGEST_LENGTH; i++)
-	{
+	for (size_t i = 0; i < SHA256_DIGEST_LENGTH; i++) {
 		sprintf(out_buff + (i * 2), "%02x", hash[i]);
 	}
-	out_buff[64] = 0;
+	out_buff[64] = '\0';
 }
 
 bool verifyProof(char* leaf, char* expectedMerkleRoot, vector<ProofNode>& proofArr) {
@@ -35,7 +30,6 @@ bool verifyProof(char* leaf, char* expectedMerkleRoot, vector<ProofNode>& proofA
 		return false;
 	}
 
-	// the merkle root should be the parent of the last part
 	char* actualMekleRoot = proofArr[proofArr.size() - 1].parent;
 
 	if (strcmp(actualMekleRoot, expectedMerkleRoot) != 0)
@@ -45,15 +39,10 @@ bool verifyProof(char* leaf, char* expectedMerkleRoot, vector<ProofNode>& proofA
 	for (int pIdx = 0; pIdx < proofArr.size(); pIdx++) {
 		ProofNode part = proofArr[pIdx];
 
-		if (strcmp(part.left, prevParent) != 0 && strcmp(part.right, prevParent) != 0)
-			return false;
+		if (strcmp(part.left, prevParent) != 0 && strcmp(part.right, prevParent) != 0)return false;
 		char* parentData = new char[65];
 		combin(part.left, part.right, parentData);
-
-		// Parent in proof is incorrect
-		if (strcmp(parentData, part.parent) != 0)
-			return false;
-
+		if (strcmp(parentData, part.parent) != 0)return false;
 		prevParent = parentData;
 	}
 
@@ -69,33 +58,27 @@ public:
 		tree = computeTree(combin, leaves, length);
 	}
 
-	size_t size() { return tree.size(); }
 	char* root() { return tree[0]; }
 
 	void calSHA256(char* inp, char out_buff[65]) {
 		unsigned char hash[SHA256_DIGEST_LENGTH];
-		SHA256_CTX sha256;
-		SHA256_Init(&sha256);
-		SHA256_Update(&sha256, inp, strlen(inp));
-		SHA256_Final(hash, &sha256);
+		SHA256_CTX digest;
+		SHA256_Init(&digest);
+		SHA256_Update(&digest, inp, strlen(inp));
+		SHA256_Final(hash, &digest);
 
-		//char buffx[65];
 		for (size_t i = 0; i < SHA256_DIGEST_LENGTH; i++)
 		{
 			sprintf(out_buff + (i * 2), "%02x", hash[i]);
 		}
-		out_buff[64] = 0;
+		out_buff[64] = '\0';
 	}
 
-
-
 	vector<char*> computeTree(void (*combineFn)(char*, char*, char*), vector<char*> leaves, size_t length) {
-		// compute nodeCount and create vector<T> tree
 		size_t nodeCount = leafCountToNodeCount(leaves.size());
 		size_t delta = nodeCount - leaves.size();
 		vector<char*> tree(nodeCount);
 
-		// deep copy
 		for (size_t i = 0; i < leaves.size(); i++) {
 			tree[delta + i] = new char[65];
 			memcpy(tree[delta + i], leaves[i], length);
@@ -104,25 +87,20 @@ public:
 
 		size_t idx = nodeCount - 1;
 		while (idx > 0) {
-			int parent = (idx - 1) / 2;
+			int parent = (idx - 1) >> 1;
 
 			tree[parent] = new char[65];
 			combineFn(tree[idx - 1], tree[idx], tree[parent]);
-			tree[parent][64] = 0;
+			tree[parent][64] = '\0';
 
 			idx -= 2;
 		}
-
 		return tree;
 	}
 
-
-
 	vector<ProofNode> proof(char* leafData) {
 		int idx = findLeaf(tree, leafData);
-		//printf("idx %d\n",idx);
-		if (idx == -1)
-			return vector<ProofNode>();
+		if (idx == -1) return vector<ProofNode>();
 		int proofArrSize = floor(log(tree.size()) / log(2));
 
 		vector<ProofNode> proof(proofArrSize);
@@ -139,31 +117,26 @@ public:
 		proof.resize(proofIdx);
 		return proof;
 	}
+
 	void pushleaf(char* leaf) {
 		pushleafworker(combin, leaf);
 	}
 
 	void pushleafworker(void (*combineFn)(char*, char*, char*), char* leaf) {
-
-		// push two
 		tree.push_back(new char[65]);
 		tree.push_back(new char[65]);
 
 		int pidx = getParent(tree, tree.size() - 1);
 
-		// push parent and newleaf
 		memcpy(tree[tree.size() - 2], tree[pidx], 65);
 		memcpy(tree[tree.size() - 1], leaf, 65);
 
-		// climb up and compute
 		int idx = tree.size() - 1;
 		while (idx > 0) {
 			idx = getParent(tree, idx);
-			//cout<<&combineFn<<'\n';
 			char* buff = new char[65];
 			combineFn(tree[getLeft(tree, idx)], tree[getRight(tree, idx)], buff);
 			tree[idx] = buff;
 		}
 	}
-
 };
