c21d477aa5cc91c3621c1c9339bd7c293b7930c3
Borpord <92631121+Borpord@users.noreply.github.com>
2023-07-27 18:39:34 +0800

    Update sm2.py

--- Project16: implement sm2 2P decrypt with real network communication/sm2.py
+++ Project16: implement sm2 2P decrypt with real network communication/sm2.py
@@ -11,7 +11,7 @@ G = (G_X, G_Y)
 h = 1
 
 
-def inv(a, n):  # Extended Euclidean Algorithm/'division' in elliptic curves
+def inv(a, n):
     lm, hm = 1, 0
     low, high = a % n, n
     while low > 1:
@@ -76,14 +76,13 @@ def get_bit_num(x):
 
 
 def generate_key():
-    private_key = int(secrets.token_hex(32), 16)
-    public_key = elliptic_multiply(private_key, G)
-    return private_key, public_key
+    sk = int(secrets.token_hex(32), 16) % N
+    pk = elliptic_multiply(sk, G)
+    return sk, pk
 
 
 def KDF(Z, klen):
-    hlen = 256  # SM3's output is 256-bit
-    n = (klen // hlen) + 1
+    n = (klen // 256) + 1
     if n >= 2 ** 32 - 1: return 'error'
     K = ''
     for i in range(n):
@@ -91,8 +90,7 @@ def KDF(Z, klen):
         tmp_b = (Z + ct).encode('utf-8')
         Kct = sm3.sm3_hash(func.bytes_to_list(tmp_b))
         K += Kct  # K is hex string
-    bit_len = 256 * n
-    K = (bin(int(K, 16))[2:]).rjust(bit_len, '0')
+    K = (bin(int(K, 16))[2:]).rjust(256 * n, '0')
     K = K[:klen]  # MSB(K, klen)
     return K
 
@@ -122,11 +120,10 @@ def SM2_enc(M, pk):
         x2 = hex(dot[0])[2:]
         y2 = hex(dot[1])[2:]
         t = KDF(x2 + y2, klen)
-        if (t != '0' * klen):
+        if t != '0' * klen:
             break
     C2 = enc_XOR(M, t)
-    tmp_b = bytes((x2 + M + y2), encoding='utf-8')
-    C3 = sm3.sm3_hash(func.bytes_to_list(tmp_b))
+    C3 = sm3.sm3_hash(func.bytes_to_list(bytes((x2 + M + y2), encoding='utf-8')))
     return (C1, C2, C3)
 
 
@@ -136,17 +133,16 @@ def SM2_dec(C, sk):
     y = C1[1]
     left = y * y % P
     right = (pow(x, 3, P) + A * x + B) % P
-    if (left != right): return """error:C1 can't satisfy EC equation"""
+    if (left != right): return "error:C1 can't satisfy EC equation"
     if C1 == 0: return 'S = hC1 =0 error'
     dot = elliptic_multiply(sk, C1)
     klen = len(C2) * 4
     x2 = hex(dot[0])[2:]
     y2 = hex(dot[1])[2:]
     t = KDF(x2 + y2, klen)
-    if t == '0' * klen: return """error: t is all '0'  """
+    if t == '0' * klen: return "error: t is all '0'"
     M = dec_XOR(C2, t)
-    tmp_b = bytes((x2 + M + y2), encoding='utf-8')
-    u = sm3.sm3_hash(func.bytes_to_list(tmp_b))
+    u = sm3.sm3_hash(func.bytes_to_list(bytes((x2 + M + y2), encoding='utf-8')))
     if u != C3: return 'error:u != C3'
     return M
 
