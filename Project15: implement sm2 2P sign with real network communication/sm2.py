922ea3bdea3021e7982eeed077b8bc87edbd1065
Borpord <92631121+Borpord@users.noreply.github.com>
2023-07-27 18:22:04 +0800

    Update sm2.py

--- Project15: implement sm2 2P sign with real network communication/sm2.py
+++ Project15: implement sm2 2P sign with real network communication/sm2.py
@@ -120,11 +120,10 @@ def SM2_enc(M, pk):
         x2 = hex(dot[0])[2:]
         y2 = hex(dot[1])[2:]
         t = KDF(x2 + y2, klen)
-        if (t != '0' * klen):
+        if t != '0' * klen:
             break
     C2 = enc_XOR(M, t)
-    tmp_b = bytes((x2 + M + y2), encoding='utf-8')
-    C3 = sm3.sm3_hash(func.bytes_to_list(tmp_b))
+    C3 = sm3.sm3_hash(func.bytes_to_list(bytes((x2 + M + y2), encoding='utf-8')))
     return (C1, C2, C3)
 
 
@@ -134,17 +133,16 @@ def SM2_dec(C, sk):
     y = C1[1]
     left = y * y % P
     right = (pow(x, 3, P) + A * x + B) % P
-    if (left != right): return """error:C1 can't satisfy EC equation"""
+    if (left != right): return "error:C1 can't satisfy EC equation"
     if C1 == 0: return 'S = hC1 =0 error'
     dot = elliptic_multiply(sk, C1)
     klen = len(C2) * 4
     x2 = hex(dot[0])[2:]
     y2 = hex(dot[1])[2:]
     t = KDF(x2 + y2, klen)
-    if t == '0' * klen: return """error: t is all '0'  """
+    if t == '0' * klen: return "error: t is all '0'"
     M = dec_XOR(C2, t)
-    tmp_b = bytes((x2 + M + y2), encoding='utf-8')
-    u = sm3.sm3_hash(func.bytes_to_list(tmp_b))
+    u = sm3.sm3_hash(func.bytes_to_list(bytes((x2 + M + y2), encoding='utf-8')))
     if u != C3: return 'error:u != C3'
     return M
 
