9ecb1f4026c855d0a489665c21f9a266129373a2
Borpord <92631121+Borpord@users.noreply.github.com>
2023-07-27 17:28:58 +0800

    Update SM2_PGP.py

--- Project14: Implement a PGP scheme with SM2/SM2_PGP.py
+++ Project14: Implement a PGP scheme with SM2/SM2_PGP.py
@@ -2,6 +2,14 @@ import random
 from Crypto.Cipher import AES
 from gmssl import sm2
 
+P = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
+A = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
+B = 0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
+N = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
+Gx = 0x32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7
+Gy = 0xBC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0
+G = [Gx, Gy]
+
 
 def PGP_encrypt(message, key):
     cryptor = AES.new(key.encode('utf-8'), AES.MODE_ECB)
@@ -30,9 +38,9 @@ def PGP_decrypt(ciphertext1, ciphertext2):
     print("Decrypted original message using session key:", plain_text.rstrip(b'\x00').decode('utf-8'))
 
 
-def key_gen(a, p, n, Gx, Gy):
-    d = random.randint(2, n - 1)
-    Q = point_mul(d, [Gx, Gy], a, p)
+def key_gen():
+    d = random.randint(2, N - 1)
+    Q = elliptic_multiply(d, [Gx, Gy])
     return d, Q
 
 
@@ -46,43 +54,33 @@ def inv(a, n):
     return lm % n
 
 
-def point_mul(k, P, a, p):
-    Q = None
-    while k > 0:
-        if k % 2 == 1:
-            Q = point_add(Q, P, a, p)
-        P = point_add(P, P, a, p)
-        k = k // 2
-
-    return Q
+def elliptic_add(a, b):
+    LamAdd = ((b[1] - a[1]) * inv(b[0] - a[0], P)) % P
+    x = (LamAdd * LamAdd - a[0] - b[0]) % P
+    y = (LamAdd * (a[0] - x) - a[1]) % P
+    return (x, y)
 
 
-def point_add(P, Q, a, p):
-    if P is None:
-        return Q
-    elif Q is None:
-        return P
+def elliptic_double(a):
+    Lam = ((3 * a[0] * a[0] + A) * inv((2 * a[1]), P)) % P
+    x = (Lam * Lam - 2 * a[0]) % P
+    y = (Lam * (a[0] - x) - a[1]) % P
+    return (x, y)
 
-    if P[0] == Q[0] and P[1] == Q[1]:
-        s = (3 * P[0] * P[0] + a) * inv(2 * P[1], p)
-    else:
-        s = (Q[1] - P[1]) * inv(Q[0] - P[0], p)
 
-    x = (s * s - P[0] - Q[0]) % p
-    y = (s * (P[0] - x) - P[1]) % p
-
-    return [x, y]
+def elliptic_multiply(ScalarHex, GenPoint):
+    if ScalarHex == 0 or ScalarHex >= N: raise Exception("Invalid Scalar/Private Key")
+    ScalarBin = str(bin(ScalarHex))[2:]
+    Q = GenPoint
+    for i in range(1, len(ScalarBin)):
+        Q = elliptic_double(Q)
+        if ScalarBin[i] == "1":
+            Q = elliptic_add(Q, GenPoint)
+    return (Q)
 
 
 if __name__ == '__main__':
-    p = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
-    a = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
-    b = 0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
-    n = 0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
-    Gx = 0x32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7
-    Gy = 0xBC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0
-    G = [Gx, Gy]
-    [private_key, public_key] = key_gen(a, p, n, Gx, Gy)
+    [private_key, public_key] = key_gen()
     private_key_hex = hex(private_key)[2:]
     public_key_hex = hex(public_key[0])[2:] + hex(public_key[1])[2:]
     sm2_crypt = sm2.CryptSM2(public_key=public_key_hex, private_key=private_key_hex)
@@ -93,7 +91,5 @@ if __name__ == '__main__':
     session_key = hex(random.randint(2 ** 127, 2 ** 128))[2:]
     print("Randomly generated symmetric encryption key:", session_key)
 
-    print()
     result1, result2 = PGP_encrypt(message, session_key)
-    print()
     PGP_decrypt(result1, result2)
